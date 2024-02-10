7da455e73000764ecdf109c602e15296fac6c4cf
Yeray Romero <yerayromerodev@gmail.com>
2024-02-10 20:32:13 +0100

    Refactored sizecheck

--- sizecheck.sh
+++ sizecheck.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-
-# Check if the user has provided a file path as an argument
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <file>"
   exit 1
 fi
-
 file_path="$1"
-
-# Check if the file exists
 if [ ! -f "$file_path" ]; then
   echo "Error: File '$file_path' not found."
   exit 1
 fi
-
 file_size_bytes=$(stat -c "%s" "$file_path")
 file_size_mb=$(echo "scale=2; $file_size_bytes / (1024 * 1024)" | bc)
 
