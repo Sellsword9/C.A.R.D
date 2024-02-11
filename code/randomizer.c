fd1f6b97dfb879f98469bef232f31d2132ef29b4
Yeray Romero <yerayromerodev@gmail.com>
2024-02-11 03:01:41 +0100

    Changed random function

--- code/randomizer.c
+++ code/randomizer.c
@@ -4,12 +4,26 @@
 char *first_names[] = {"John", "Jane", "Michael", "Emily", "David", "Sarah", "James", "Mary", "Robert", "Jennifer"};
 char *last_names[] = {"Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor"};
 char *streets[] = {"Main St", "Elm St", "Oak St", "Maple Ave", "Cedar Ln", "Pine St", "Washington St", "Lake Rd", "Park Ave", "Sunset Blvd"};
+unsigned int xorshift_state = 123456789;
+
+void set_seed(unsigned int seed)
+{
+  xorshift_state = seed;
+}
+
+unsigned int xorshift32()
+{
+  xorshift_state ^= xorshift_state << 13;
+  xorshift_state ^= xorshift_state >> 17;
+  xorshift_state ^= xorshift_state << 5;
+  return xorshift_state;
+}
 
 int random_int(int min, int max)
 {
-  return min + rand() % (max - min + 1);
+  unsigned int range = (unsigned int)(max - min + 1);
+  return (int)(min + xorshift32() % range);
 }
-
 void generate_fake_phone(char *phone)
 {
   sprintf(phone, "%d-%d-%d",
