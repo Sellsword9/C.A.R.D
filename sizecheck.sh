431f7bf942eacf0bc1e5d2365712e3e461fd644b
Yeray Romero <yerayromerodev@gmail.com>
2024-02-11 02:39:42 +0100

    Safechecks removed

--- sizecheck.sh
+++ sizecheck.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <file>"
-  exit 1
-fi
-file_path="$1"
-if [ ! -f "$file_path" ]; then
-  echo "Error: File '$file_path' not found."
-  exit 1
-fi
 file_size_bytes=$(stat -c "%s" "$file_path")
 file_size_mb=$(echo "scale=2; $file_size_bytes / (1024 * 1024)" | bc)
-
 echo "$file_size_mb MB."
